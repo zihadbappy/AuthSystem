618b5d32801b11da60fd6a53b95260e0ec445aeb
Zihad Bappy <zihadbappy@gmail.com>
2020-08-17 03:14:59 +0600

    2nd commit | signup, login markup done

--- routes/users.js
+++ routes/users.js
@@ -2,8 +2,12 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/signup/', function(req, res, next) {
+  res.render('signup',{title:'EZget | signup'});
+});
+
+router.get('/login/', function(req, res, next) {
+  res.render('login',{title:'EZget | login'});
 });
 
 module.exports = router;
